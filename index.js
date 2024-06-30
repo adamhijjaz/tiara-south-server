0d12c5725d7e197386133003f0d6083284269a54
Nazrin Amirrullah bin Zaidi <147366697+nazrinamir@users.noreply.github.com>
2024-06-30 19:21:51 +0800

    added user deleted Posts

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+require("dotenv").config();
 
 const db = require("./models");
+console.log("Running");
+const userRouter = require("./routes/Users");
+app.use("/auth", userRouter);
 
-
-// Routers migh change later 
-const postRouter = require("./routes/Posts");
-app.use("/posts", postRouter);
-
-let port = process.env.PORT || 3001;
-db.sequelize.sync().then(() => {
-  app.listen( port, () => {
-    console.log("Server running on port: " + port);
-  });
+db.sequelize.sync().then(() =>{
+    app.listen(process.env.PORT || 3001, () => {
+        console.log("Serever run on port 3001");
+    });
 });
+
